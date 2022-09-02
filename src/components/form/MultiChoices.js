ce550b5cbb3b2e65aeb4dc5fc2dd054604055e64
kaoutar-ou <kaoutar.oubenaddi@gmail.com>
2022-09-02 02:43:58 +0100

    KO : Rating in progress ...

--- src/components/form/MultiChoices.js
+++ src/components/form/MultiChoices.js
@@ -5,10 +5,10 @@ function MultiChoices() {
     <div className="relative">
       <div className="w-full flex flex-row">
         <div className="w-full m-5 rounded-2xl shadow-xl break-all outline-dotted outline-1 outline-gray-500 pb-6 bg-gradient-to-r from-gray-300 to-gray-200">
-          <div className="bg-gray-100 rounded-xl m-4 p-1 outline-dotted outline-1 outline-gray-500 hover:outline-offset-2 hover:bg-white">Choice</div>
-          <div className="bg-gray-100 rounded-xl m-4 p-1 outline-dotted outline-1 outline-gray-500 hover:outline-offset-2 hover:bg-white">Choice</div>
-          <div className="bg-gray-100 rounded-xl m-4 p-1 outline-dotted outline-1 outline-gray-500 hover:outline-offset-2 hover:bg-white">Choice</div>
-          <div className="bg-gray-100 rounded-xl m-4 p-1 outline-dotted outline-1 outline-gray-500 hover:outline-offset-2 hover:bg-white">Choice</div>
+          <div className="bg-gray-100 rounded-xl m-4 p-1 outline-dotted outline-1 outline-gray-500 hover:outline-offset-2 hover:bg-white cursor-pointer">Choice</div>
+          <div className="bg-gray-100 rounded-xl m-4 p-1 outline-dotted outline-1 outline-gray-500 hover:outline-offset-2 hover:bg-white cursor-pointer">Choice</div>
+          <div className="bg-gray-100 rounded-xl m-4 p-1 outline-dotted outline-1 outline-gray-500 hover:outline-offset-2 hover:bg-white cursor-pointer">Choice</div>
+          <div className="bg-gray-100 rounded-xl m-4 p-1 outline-dotted outline-1 outline-gray-500 hover:outline-offset-2 hover:bg-white cursor-pointer">Choice</div>
         </div>
       </div>
       <button className="rounded-full bg-gray-100 outline-dotted outline-1 outline-gray-500 hover:outline-offset-2 w-10 h-10 absolute -mt-10 -ml-5 hover:bg-teal-500 hover:text-white">
@@ -17,5 +17,8 @@ function MultiChoices() {
     </div>
   );
 }
+
+// TODO votre email exist already .. vouz avez recu email du meet
+
 
 export default MultiChoices;

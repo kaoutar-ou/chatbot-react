06ed342dcc61f2cfed300898a3c7fca97def398a
kaoutar-ou <kaoutar.oubenaddi@gmail.com>
2022-09-08 14:41:31 +0100

    KO : Adding language support .. IP ...

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export default axios.create({
     baseURL: "http://localhost:8000/",
-    timeout: 100000,
+    timeout: 10000,
     headers: {
         'Content-Type': 'application/json',
         'Access-Control-Allow-Origin': 'http://localhost:3000',
